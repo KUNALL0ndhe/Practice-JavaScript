ddab2a545f114a1f2e403c966dfc7d41c5a14472
KUNALL0ndhe <167422947+KUNALL0ndhe@users.noreply.github.com>
2024-06-21 15:08:52 +0530

    01_variables done.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accId = 1234;
+let ename = "Kunal";
+var city = "mumbai";
+password = "*****";
+let accState;
+
+//accId = 4321;// not allowed;
+/*
+Prefer Note :
+Not to use var keyword to initialize any variable ,
+Because it does not support scope functionality,
+*/
+ename = "londhe";
+city = "New York";
+password = "######";
+
+console.table([accId, ename, city, password, accState ]);

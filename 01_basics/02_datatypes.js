8e53988ec54ca64765b69f640b96d22043587379
KUNALL0ndhe <167422947+KUNALL0ndhe@users.noreply.github.com>
2024-06-22 17:11:11 +0530

    02_datatypes.

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = "abc";
+let c = true;
+
+let d;
+let e = null;
+
+console.log(typeof(a));//number
+console.log(typeof(b));//string
+console.log(typeof(c));//boolean
+console.log(typeof(d));//undefined
+console.log(typeof(e));//object
+

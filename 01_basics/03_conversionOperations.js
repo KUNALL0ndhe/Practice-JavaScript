795750666a8c235eb4c34dc1e6796a24fe893088
KUNALL0ndhe <167422947+KUNALL0ndhe@users.noreply.github.com>
2024-06-29 13:45:13 +0530

    Conversion Operation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+//let score = "33";
+
+//console.log(typeof score); // string
+
+//let no = Number(score);
+//console.log(no);
+//console.log(typeof score); // string
+//console.log(typeof no); // number conversion
+
+let bool = null;
+
+console.log(typeof bool); // string
+console.log(Boolean(bool)); // 
+console.log(bool); 
+

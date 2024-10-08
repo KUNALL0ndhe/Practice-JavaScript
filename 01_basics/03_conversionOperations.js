550a36e6121988febc1bf16381ef4c573f0c5635
unknown <kunallondhex@gmail.com>
2024-10-08 13:15:57 +0530

    Conversion datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -9,7 +9,23 @@
 
 let bool = null;
 
-console.log(typeof bool); // string
-console.log(Boolean(bool)); // 
-console.log(bool); 
+//console.log(typeof bool); // Object
+//console.log(Boolean(bool)); // false
+//console.log(bool); 
 
+/* 
+1 => true
+"kunal" => true
+"" => false
+null => object, false 
+*/
+
+let score =  NaN
+
+let convertScore = String(score);
+console.log(convertScore);
+console.log(typeof(convertScore));
+
+/*
+everything gets converted to string
+*/

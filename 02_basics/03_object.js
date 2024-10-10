8ecc519a871463b7bc1d36a1c50c06d19dec3816
unknown <kunallondhex@gmail.com>
2024-10-10 23:05:35 +0530

    Object Methods part 2

---
+++ 02_basics/03_object.js
@@ -0,0 +1,19 @@
+let odd = { a : "1", c: "3" , e: "5" };
+let even = { b : "2", d: "4", f: "6"};
+
+// let merge = Object.assign({}, odd, even); // one way to merge the object
+let merge = { ...odd,...even}
+console.log(merge); // Most common method to merge the object 
+
+const tinderUSer = {
+    id: "123abc",
+    name:"Kunal",
+    isLoggedIn:true,
+    point: null,
+};
+
+console.log(Object.keys(tinderUSer)); // gives all the Keys in the given object
+console.log(Object.values(tinderUSer)); // give all the values in the given object
+console.log( Object.entries(tinderUSer)); // it merge the key and the value into and array and returns an array of object
+console.log(tinderUSer.hasOwnProperty("view")); // returns boolean , while the propertyKey is provided
+// in the hasOwnProperty it checks whether the key is present or not

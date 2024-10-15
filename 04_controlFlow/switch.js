18d309aa2ad09e054178464fa5e743151e1e7007
unknown <kunallondhex@gmail.com>
2024-10-15 18:14:16 +0530

    Control Floe Switch case.

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,35 @@
+// switch case is used to check one variable with each rather than the if condition
+
+let month = 'March'
+
+switch (month) {
+    case "Jan":
+        console.log(`you are in the ${month} block`);
+        
+        break;
+    case "FEb":
+        console.log(`you are in the ${month} block`);
+        
+        
+        break;
+    case "MArch":
+        console.log(`you are in the ${month} block`);
+        
+        
+        break;
+    case "April":
+        console.log(`you are in the ${month} block`);
+        
+        
+        break;
+    case "May":
+        console.log(`you are in the ${month} block`);
+        
+        
+        break;
+
+    default:
+        console.log(`you are in the ${month} block`);
+        
+        break;
+}

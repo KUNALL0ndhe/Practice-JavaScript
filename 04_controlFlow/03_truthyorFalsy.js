1f0eae17efec90c4fd65c2d74cbe9241b757a4d5
unknown <kunallondhex@gmail.com>
2024-10-15 18:30:11 +0530

    Control Floe TruthyFalse.

---
+++ 04_controlFlow/03_truthyorFalsy.js
@@ -0,0 +1,39 @@
+let myvalue = '';
+
+// if (myvalue) {
+//     console.log('In the truthy block');
+// }
+// {
+//     console.log('in he common block');
+// }
+
+/* falsy values
+
+false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+truthy values
+"0", 'false', " ", [], {}, function(){}
+
+*/
+
+// const myArray = {};
+
+// if(Object.keys(myArray).length == 0){
+//     console.log('Object is empty');
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+
+
+// let val = 5 ?? 10;
+// let val2 = null ?? 5;
+// let val3 = undefined ??  20;
+// let val4 = null ?? undefined ?? 100;
+
+// console.log(val,val2,val3,val4);
+
+
+let iceTea = "90";
+
+iceTea >= 100 ? console.log(`oh it is costly`) : console.log(`resonable`);
+

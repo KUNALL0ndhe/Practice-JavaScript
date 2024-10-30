2acaa9fba1cb89a45c678192db9fe579b0f6c260
unknown <kunallondhex@gmail.com>
2024-10-30 13:49:50 +0530

    Algo 2 in fibonacci

--- DSA/fibonacci.js
+++ DSA/fibonacci.js
@@ -1,11 +1,34 @@
-const finonacci = (n) => {
-    let fib = [0,1];
+// const finonacci = (n) => {
+//     let fib = [0,1];
 
-    for ( i = 2; i<=n ; i++) {
+//     for ( i = 2; i<=n ; i++) {
 
-        fib[i] = fib[i-1] + fib[i-2];
-    };
-    return fib
+//         fib[i] = fib[i-1] + fib[i-2];
+//     };
+//     return fib
+// };
+
+// console.log(finonacci(3), 'This is fibonacci')
+
+
+
+const funcFibonacci = (num) => {
+let Myfibonacci = [0,1]
+
+for (i = 2 ; i <= num ; i++) {
+    Myfibonacci[i] = Myfibonacci[i-1] + Myfibonacci[i-2];
+
+};
+return Myfibonacci;
 };
 
-console.log(finonacci(3), 'This is fibonacci')
+console.log(funcFibonacci(5))
+
+
+/**
+ * 
+ * Here the BIG O is O(n)
+ * when the input N increases correspondingly the runtine also increases.
+ * 
+ * the worst case scenerio -> O(n)
+ */
